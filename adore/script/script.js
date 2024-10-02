069cf44196cb7e9d7196a75bc0667472dc25787d
jose-Almir-lira <jose.almir2121@gmail.com>
2024-10-02 16:25:46 -0300

    Update script.js

--- adore/script/script.js
+++ adore/script/script.js
@@ -1,9 +1,9 @@
 const slide = document.getElementById('slide');
 const thumbnailsContainer = document.getElementById('thumbnails');
-const images = ['Davi.jpg','Diego.jpg','Kaka.jpg','Ronaldo-teclado.jpg']; // Adicione os nomes das imagens aqui
+const images = ['./Davi.jpg','./Diego.jpg','./Kaka.jpg','./Ronaldo-teclado.jpg']; // Adicione os nomes das imagens aqui
 
 // Lista de imagens (adapte conforme suas imagens)
-const imageNames = ['Davi.jpg','Diego.jpg','Kaka.jpg','Ronaldo-teclado.jpg']; // Adicione mais imagens aqui
+const imageNames = ['./Davi.jpg','./Diego.jpg','./Kaka.jpg','./Ronaldo-teclado.jpg']; // Adicione mais imagens aqui
 
 imageNames.forEach(name => {
     const img = document.createElement('img');
